f4a3da050c6a9ca77739f9a7e1b6dc8893edbae8
Valeriya <wal.now@yandex.ru>
2022-12-17 17:17:20 +0300

    h

---
+++ teacher.py
@@ -0,0 +1,14 @@
+import operations
+
+def menu_teach():
+    complete = False
+    print('Выберете действия: 1 - поставить оценку; 2 - посмотреть оценки')
+    choice = int(input())
+    
+    if choice == 1:
+        score = read_grade()
+
+
+
+
+    
