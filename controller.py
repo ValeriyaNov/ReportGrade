db132caa7261023fca48c6ca897ae12dc47dd95b
Valeriya <wal.now@yandex.ru>
2022-12-21 22:51:10 +0300

    d

---
+++ controller.py
@@ -0,0 +1,23 @@
+import Identifikation
+import operations
+import teacher
+
+
+print('Электронный табель оценок')
+name_status = Identifikation.identifikation()
+
+while True:
+    if name_status[1] == 'преподаватель':
+        teacher.menu_teach()
+    elif name_status[1] == 'студент':
+        operations.find_grade_student(name_status[0])
+    print('Хотите продолжить? Если нет, то нажмите 1')
+    n = input()
+    if n == '1':
+        break
+    else:
+        continue
+
+
+    
+
