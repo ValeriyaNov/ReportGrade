df19c4ddfe2a657e015317983be579707ae624f5
Valeriya <wal.now@yandex.ru>
2022-12-21 22:59:28 +0300

    t

--- teacher.py
+++ teacher.py
@@ -10,19 +10,22 @@ def menu_teach():
     choic = input()
     if choic == '1':
         score = operations.read_grade()
-        print('Если хотите записать данные, то нажмите 1, если не, то -2')
-        answer = int(input())
-        comm = False
-        while not comm:
+        while True:
+            print('Если хотите записать данные, то нажмите 1, если не, то -2')
+            answer = int(input())
+        
+        
             if answer == 1:
                 with open('Grade.csv', 'a', encoding='UTF-8') as f:
                     f_writ = csv.writer(f, delimiter = "|")
                     #f_writ.writerow(' ')
                     f_writ.writerow(score)
                     ''', lineterminator="\n"'''
-                    comm = True
+                break    
             elif answer == 2:
-                comm = True
+                break
+            else:
+                print('Некорректный ввод, попробуйте заново!')
             # if not comm:
             #     print('Ошибка ввода, введите заново!')
     elif choic == '2':
@@ -35,6 +38,6 @@ def menu_teach():
 menu_teach()
 
 
-import os
-print (os.listdir(os.getcwd()))
+# import os
+# print (os.listdir(os.getcwd()))
 
