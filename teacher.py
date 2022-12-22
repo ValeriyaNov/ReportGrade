aa58fdd0c6f68ba93304e213e85e32eb1f1240eb
Valeriya <wal.now@yandex.ru>
2022-12-22 11:42:39 +0300

    меню для преподавателя

--- teacher.py
+++ teacher.py
@@ -10,32 +10,33 @@ def menu_teach():
     choic = input()
     if choic == '1':
         score = operations.read_grade()
-        while True:
+        complet = False
+        while not complet:
             print('Если хотите записать данные, то нажмите 1, если не, то -2')
             answer = int(input())
         
-        
             if answer == 1:
                 with open('Grade.csv', 'a', encoding='UTF-8') as f:
-                    f_writ = csv.writer(f, delimiter = "|")
+                    f_writ = csv.writer(f, delimiter = "|", lineterminator="\n" )
                     #f_writ.writerow(' ')
                     f_writ.writerow(score)
-                    ''', lineterminator="\n"'''
-                break    
+                    f_writ.writerow(' ')
+                    
+                complet = True   
             elif answer == 2:
-                break
+                complet = True
             else:
                 print('Некорректный ввод, попробуйте заново!')
-            # if not comm:
-            #     print('Ошибка ввода, введите заново!')
+            
     elif choic == '2':
         operations.find_grade_teacher()
     else:
         print('Некорректный ввод')
+    
 
         
     
-menu_teach()
+
 
 
 # import os
